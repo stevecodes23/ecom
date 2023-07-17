1c5098987afb83e5d309c2a555ee0f851b971625
stevecodes23 <stevemsih2308@gmail.com>
2023-07-17 11:24:01 +0530

    profile added

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { CreateProfileDto } from './dto/profile.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('profiles')
 export class ProfileController {
@@ -8,6 +9,9 @@ export class ProfileController {
         private readonly profileService:ProfileService,
       ){}
 
+@ApiResponse({ status: 201, description: 'The profile has been successfully created.'})
+@ApiOperation({ summary: 'create profile using this  ' })
+@ApiTags('profile')
  @Post()
  createProfile(@Body() data: CreateProfileDto) {
     return this.profileService.createProfile(data);
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,5 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/services/prisma.services';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService { constructor(
+    private readonly prisma: PrismaService  
+){}
+
+async createProfile(data:any):Promise<any> {
+    return this.prisma.profile.create({
+        data:{
+            user_id:data.user_id,
+            alternate_number:data.alternate_number,
+            image_id:data.image_id,
+            gender:data.gender,
+  
+        }
+    })
+}
+}
 
