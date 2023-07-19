7e07ae9976755c1a031208ce6a8ff2e863a14036
stevecodes23 <stevemsih2308@gmail.com>
2023-07-19 19:32:30 +0530

    added

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { PrismaService } from 'src/services/prisma.services';
+import { CategoryService } from './category.service';
+import { categoryDto } from './dto/category.dto';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+    constructor(private readonly prisma:PrismaService,
+                private readonly categoryService:CategoryService){}
+
+@ApiResponse({ status: 201, description: 'The product has been successfully created.'})
+@ApiOperation({ summary: 'add product using this ' })
+@ApiTags('product')
+@Post()
+async addCategory(@Body()data:categoryDto){
+    return this.categoryService.addCategory(data)
+}
+}
+
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/services/prisma.services';
+import { categoryDto } from './dto/category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    constructor(
+        private readonly prisma:PrismaService
+    ){}
+
+    async addCategory(data:any):Promise<any>{
+        const category = await this.prisma.category.create({
+            data:{
+                name:data.name,
+                parent_id:data.parent_id,
+                image_id:data.image_id,
+            }
+        })
+        return {category:category}
+    }
+}
