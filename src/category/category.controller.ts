2a0ae0a1ea13487bd635f0d06422f146a8be77bf
stevecodes23 <stevemsih2308@gmail.com>
2023-07-20 12:40:26 +0530

    category added

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,20 +1,46 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, DefaultValuePipe, Delete, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PrismaService } from 'src/services/prisma.services';
 import { CategoryService } from './category.service';
 import { categoryDto } from './dto/category.dto';
+import { API_CONSTANTS } from 'src/utils/constants/perPage';
 
 @Controller('category')
 export class CategoryController {
     constructor(private readonly prisma:PrismaService,
                 private readonly categoryService:CategoryService){}
 
-@ApiResponse({ status: 201, description: 'The product has been successfully created.'})
-@ApiOperation({ summary: 'add product using this ' })
-@ApiTags('product')
+@ApiResponse({ status: 201, description: 'The category has been successfully created.'})
+@ApiOperation({ summary: 'add category using this ' })
+@ApiTags('category')
 @Post()
-async addCategory(@Body()data:categoryDto){
+async addCategory(@Body()data:categoryDto):Promise<any>{
     return this.categoryService.addCategory(data)
 }
+@ApiResponse({ status: 201, description: 'The category has been successfully deleted.'})
+@ApiOperation({ summary: 'delete category using this ' })
+@ApiTags('category')
+@Delete("/:id")
+async deleteCategory(@Param('id')id :number):Promise<any>{
+    return this.categoryService.deleteCategory(id)
+}
+@ApiResponse({ status: 201, description: 'The category list has been generated.'})
+@ApiOperation({ summary: 'get category list using this ' })
+@ApiTags('category')
+@Get()
+async getAllCategory(@Query('page', new DefaultValuePipe(0), ParseIntPipe)
+page: number,
+@Query('per_page', new DefaultValuePipe(API_CONSTANTS.perPage), ParseIntPipe)
+perPage:number):Promise<any>{
+    return this.categoryService.getAllCategory(page,perPage)
+}
+@ApiResponse({ status: 201, description: 'The subcategory list has been generated.'})
+@ApiOperation({ summary: 'get subcategory list using this ' })
+@ApiTags('category')
+@Get("/:id/subcategories")
+async getSubCategory(@Param('id')id:number):Promise<any>{
+    return this.categoryService.getSubCategory(id)
+}
+
 }
 
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/services/prisma.services';
 import { categoryDto } from './dto/category.dto';
+import { Utility } from 'src/utils/utility';
 
 @Injectable()
 export class CategoryService {
@@ -17,5 +18,29 @@ export class CategoryService {
             }
         })
         return {category:category}
+    }
+    async deleteCategory(id:number):Promise<any>{
+        const deleted_category = await this.prisma.category.delete({
+            where:{
+                id:id
+            }})
+            return {deleted_category:deleted_category}
+    }
+    async getAllCategory(page:number,perPage:number):Promise<any>{
+        const totalCount= await this.prisma.product.count({})
+        const list= await this.prisma.product.findMany({
+            skip:page*perPage,
+            take:perPage,
+        })
+     return Utility.getPaginatedFormatData(list,totalCount,page,perPage)
+    
+    }
+    async getSubCategory(id:number):Promise<any>{
+        const subcategories= await this.prisma.category.findMany({
+            where:{
+                parent_id:id
+            }
+        })
+        return{subcategories:subcategories}
     }
 }
