1a60c7d8ee6810d140044a59dbec8ca38a0c5ba2
stevecodes23 <stevemsih2308@gmail.com>
2023-07-20 12:51:00 +0530

    category added

--- src/category/dto/category.dto.ts
+++ src/category/dto/category.dto.ts
@@ -1,3 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional } from "class-validator";
+
 export class categoryDto{
-    
+  
+    @ApiProperty({
+        type:String,
+        description: 'name is a required property',
+      })
+    @IsNotEmpty()
+    name:string
+
+    @ApiProperty({
+        type:Number,
+        description: 'parent_id is a optional property',
+      })
+    @IsOptional()
+    parent_id:number
+
+    @ApiProperty({
+        type:Number,
+        description: 'image_id is a required property',
+      })
+    @IsNotEmpty()
+    image_id:number
 }
