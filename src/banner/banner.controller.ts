abc4908f1f43330a432f77b33502091bb534a002
stevecodes23 <stevemsih2308@gmail.com>
2023-07-25 18:09:03 +0530

    schema changes

--- src/banner/banner.controller.ts
+++ src/banner/banner.controller.ts
@@ -6,46 +6,46 @@ import { CreateBannerResponseDto, CreateBannerDto, DeleteBannerResponseDto, GetA
 
 @Controller('banner')
 export class BannerController {
-    constructor(private readonly bannerService:BannerService){}
-@ApiResponse({ status: 201, description: 'The banner has been successfully created.',type:CreateBannerResponseDto})
-@ApiOperation({ summary: 'add banner using this ' })
-@ApiTags('banner')
-@Post()
-async addBanner(@Body()data:CreateBannerDto):Promise<any>{ 
-    return this.bannerService.addBanner(data)
+    constructor(private readonly bannerService: BannerService) { }
+    @ApiResponse({ status: 201, description: 'The banner has been successfully created.', type: CreateBannerResponseDto })
+    @ApiOperation({ summary: 'add banner using this ' })
+    @ApiTags('banner')
+    @Post()
+    async addBanner(@Body() data: CreateBannerDto): Promise<any> {
+        return this.bannerService.addBanner(data)
     }
-@ApiResponse({ status: 201, description: 'The banner list has been successfully generated.',type:GetAllBannerDto})
-@ApiOperation({ summary: 'get banner using this ' })
-@ApiTags('banner')
-@Get()
-async getAllBanner(@Query('page', new DefaultValuePipe(0), ParseIntPipe)
-page: number,
-@Query('per_page', new DefaultValuePipe(API_CONSTANTS.perPage), ParseIntPipe)
-perPage:number):Promise<any>{
-return this.bannerService.getAllBanner(page,perPage)
-}
+    @ApiResponse({ status: 201, description: 'The banner list has been successfully generated.', type: GetAllBannerDto })
+    @ApiOperation({ summary: 'get banner using this ' })
+    @ApiTags('banner')
+    @Get()
+    async getAllBanner(@Query('page', new DefaultValuePipe(0), ParseIntPipe)
+    page: number,
+        @Query('per_page', new DefaultValuePipe(API_CONSTANTS.perPage), ParseIntPipe)
+        perPage: number): Promise<any> {
+        return this.bannerService.getAllBanner(page, perPage)
+    }
 
-@ApiResponse({ status: 201, description: 'The banner has been successfully deleted.',type:DeleteBannerResponseDto})
-@ApiOperation({ summary: 'delete banner using this ' })
-@ApiTags('banner')
-@Delete('/:id')
-async deleteBanner(@Param('id')id:number):Promise<any>{
-    return this.bannerService.deleteBanner(id)
-}
-@ApiResponse({ status: 201, description: 'The banner status has been successfully changed.',type:ToggleStatusResponseDto})
-@ApiOperation({ summary: 'change banner status using this ' })
-@ApiTags('banner')
-@Patch('/:id')
-async toggleStatus(@Param('id')id:number):Promise<any>{
-    return this.bannerService.toggleStatus(id)
-}
-@ApiResponse({ status: 201, description: 'The banner has been successfully updated ',type:UpdateResponseDto})
-@ApiOperation({ summary: 'update banner ' })
-@ApiTags('banner')
-@Put('/:id')
-async updateBanner(@Param('id') id:number,@Body()data:UpdateBannerDto):Promise<any>{
-    return this.bannerService.updateBanner(id,data)
-}
+    @ApiResponse({ status: 201, description: 'The banner has been successfully deleted.', type: DeleteBannerResponseDto })
+    @ApiOperation({ summary: 'delete banner using this ' })
+    @ApiTags('banner')
+    @Delete('/:id')
+    async deleteBanner(@Param('id') id: number): Promise<any> {
+        return this.bannerService.deleteBanner(id)
+    }
+    @ApiResponse({ status: 201, description: 'The banner status has been successfully changed.', type: ToggleStatusResponseDto })
+    @ApiOperation({ summary: 'change banner status using this ' })
+    @ApiTags('banner')
+    @Patch('/:id')
+    async toggleStatus(@Param('id') id: number): Promise<any> {
+        return this.bannerService.toggleStatus(id)
+    }
+    @ApiResponse({ status: 201, description: 'The banner has been successfully updated ', type: UpdateResponseDto })
+    @ApiOperation({ summary: 'update banner ' })
+    @ApiTags('banner')
+    @Put('/:id')
+    async updateBanner(@Param('id') id: number, @Body() data: UpdateBannerDto): Promise<any> {
+        return this.bannerService.updateBanner(id, data)
+    }
 
 
 }
